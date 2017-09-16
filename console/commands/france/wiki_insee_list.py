77f601ffb00837419b09772ce1558b23466da845
bpteam <10122202z3223544z>
2017-09-16 03:29:07 +0300

    merge requests

--- console/commands/france/wiki_insee_list.py
+++ console/commands/france/wiki_insee_list.py
@@ -44,7 +44,7 @@ file_name_insee_list = opts.s if use_insee_list else './WorkBaseFile/BaseCommune
 def update_meta(url, request, document):
     actual_doc = document.get_document()
     actual_doc.update(url=url)
-    added_requests = [tuple(x) for x in actual_doc.get('requests', ())]
+    added_requests = [(tuple(x) if isinstance(x, list) else x) for x in actual_doc.get('requests', ())]
     added_requests.append(request)
     actual_doc.update(requests=list(set(added_requests)))
     document.update(actual_doc)
