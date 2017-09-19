1d51a9edeed6e2392b64f565048a88e72d386626
bpteam <10122202z3223544z>
2017-09-20 02:35:43 +0300

    draft for gmaps worker

---
+++ console/task_generator/italy/gmap_address.py
@@ -0,0 +1,31 @@
+from lib.job.storage.MongoDB import MongoDB as Storage
+from lib.job.map.google.AddressTask import AddressTask
+from lib.config.Yaml import Yaml as Config
+from lib.factory.StorageLocation import StorageLocation as DocFactory
+
+country = 'Italia'
+lst_address = []
+
+config = Config('./config/config.yml').get('mongodb')
+
+job_list = Storage(AddressTask.TYPE, config)
+
+factory = DocFactory(config)
+wiki = factory.wiki_collection()
+
+filter = {
+    'name': {'$exists': True, '$not': {'$size': 0}},
+    'admin_hierarchy': {'$elemMatch': {'name': country}}
+}
+
+for search_result in wiki.find(filter):
+    try:
+        new_address = ''
+        for admin in search_result['admin_hierarchy']:
+            new_address += admin['name']
+            if new_address not in lst_address:
+                lst_address.append(new_address)
+                job_list.add(new_address)
+            new_address += ', '
+    except KeyError:
+        pass
---
+++ console/task_generator/italy/gmap_position.py
@@ -0,0 +1,30 @@
+from lib.job.storage.MongoDB import MongoDB as Storage
+from lib.job.map.google.PositionTask import PositionTask
+from lib.config.Yaml import Yaml as Config
+from lib.factory.StorageLocation import StorageLocation as DocFactory
+
+country = 'Italia'
+
+config = Config('./config/config.yml').get('mongodb')
+
+job_list = Storage(PositionTask.TYPE, config)
+
+
+factory = DocFactory(config)
+wiki = factory.wiki_collection()
+
+filter = {
+    'name': {'$exists': True, '$not': {'$size': 0}},
+    'admin_hierarchy': {'$elemMatch': {'name': country}}
+}
+
+objects = wiki.find(filter)
+for obj in objects:
+    try:
+        position = obj['center']
+        position_list = ([pos for pos in position.values() if isinstance(pos, float)])
+        if len(position_list) == 2:
+            job_list.add(position)
+    except KeyError:
+        continue
+
