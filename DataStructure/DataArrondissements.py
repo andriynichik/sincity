101591d23f4e762c04b3dc49dc6b945a05403bd9
romiss <romissevd@gmail.com>
2017-09-20 23:35:25 +0300

    added functions form wiki parser csv file

--- DataStructure/DataArrondissements.py
+++ DataStructure/DataArrondissements.py
@@ -26,6 +26,17 @@ def different_languages(row):
     return in_18
 
 
+def admin_division(row, wiki_admin):
+    admin_hierarchy = []
+    n = 0
+    for admin_div in wiki_admin:
+        n += 1
+        name = row.get(admin_div)
+        if name:
+            admin_hierarchy.append({'name': name, 'type': 'ADMIN_LEVEL_{}'.format(n)})
+    return admin_hierarchy
+
+
 with open('arrondissements_25_08_17_cards.csv', encoding='utf-8') as csvfile:
     for line in csv.DictReader(csvfile, delimiter='\t'):
 
@@ -36,28 +47,25 @@ with open('arrondissements_25_08_17_cards.csv', encoding='utf-8') as csvfile:
         }
 
 
-        name_pays = line.get('W_Pays')
-        name_region = line.get('W_Region')
-        name_departement = line.get('W_Departement')
-        name_arrondissement = line.get('Wiki_name_arrondissement')
-
-        admin_hierarchy = [
-            {'name': name_pays, 'type': 'ADMIN_LEVEL_1'},
-            {'name': name_region, 'type': 'ADMIN_LEVEL_2'},
-            {'name': name_departement, 'type': 'ADMIN_LEVEL_3'},
-            {'name': name_arrondissement, 'type': 'ADMIN_LEVEL_4'},
+        wiki_name_admins_keys = ['W_Pays',
+                                 'W_Region',
+                                 'W_Departement',
+                                 'Wiki_name_arrondissement',
         ]
+
+        admin_hierarchy = admin_division(line, wiki_name_admins_keys)
 
         in_18 = different_languages(line)
 
         dct['wiki'].update({'in_18': in_18})
-        dct['wiki'].update({'name': name_arrondissement})
 
-        for admin_division in admin_hierarchy:
-            if admin_division['name'] == dct['wiki']['name']:
-                dct['wiki'].update({'type': admin_division['type']})
+        name_arrondissement = line.get(wiki_name_admins_keys[-1])
+        if name_arrondissement:
+            dct['wiki'].update({'name': name_arrondissement})
+            for ad in admin_hierarchy:
+                if ad['name'] == dct['wiki']['name']:
+                    dct['wiki'].update({'type': ad['type']})
 
-### wiki
         wiki_url = get_url(line)
         if wiki_url:
             url = wiki_france + wiki_url
@@ -65,12 +73,25 @@ with open('arrondissements_25_08_17_cards.csv', encoding='utf-8') as csvfile:
             dct['wiki'].update({'url': url})
             dct['wiki'].update({'code': code})
 
+        capital = line.get('W_Chef-lieu')
+        if capital:
+            dct['wiki'].update({'capital': capital})
+
+        lat = line.get('Wiki_Coordinates_lat')
+        lon = line.get('Wiki_Coordinates_lon')
+        if lat is not None and lon is not None:
+            dct['wiki'].update({'center': {
+                                            'lat': float(lat),
+                                            'lon': float(lon),
+                                          }
+                               })
 
 
-        code_arrondissements = line.get('I_Code_Arrondissements')
-        req = (
-            'https://fr.wikipedia.org/w/index.php?search="code+arrondissement+{}"'.format(code_arrondissements)
-        )
+
+        # code_arrondissements = line.get('I_Code_Arrondissements')
+        # req = (
+        #     'https://fr.wikipedia.org/w/index.php?search="code+arrondissement+{}"'.format(code_arrondissements)
+        # )
 
         print(dct)
 
