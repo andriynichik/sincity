e48996366942c23766882a9d1ec00b2185ce35aa
bpteam <bpteam22@gmail.com>
2017-09-29 01:56:24 +0300

    reparser wiki for italy

--- console/task_generator/italy/wiki_page_from_wiki.py
+++ console/task_generator/italy/wiki_page_from_wiki.py
@@ -1,14 +1,22 @@
 from lib.job.storage.MongoDB import MongoDB as Storage
-from lib.job.map.google.PositionTask import PositionTask
+from lib.job.wiki.PageTask import PageTask
 from lib.config.Yaml import Yaml as Config
 from lib.factory.StorageLocation import StorageLocation as DocFactory
+import pandas as pd
+import urllib.parse
 
 country = 'Italia'
+lst_address = []
+
+region_index = 1
+provincia_index = 3
+comune_index = 5
+localita_index = 9
+
 
 config = Config('./config/config.yml').get('mongodb')
 
-job_list = Storage(PositionTask.get_name(country), config)
-
+job_list = Storage(PageTask.get_name(country), config)
 
 factory = DocFactory(config)
 wiki = factory.wiki_collection()
@@ -21,9 +29,8 @@ filter = {
 objects = wiki.find(filter)
 for obj in objects:
     try:
-        position = obj['center']
-        if len(position) == 2:
-            job_list.add(position)
+        url = obj['url']
+        if url:
+            job_list.add(url)
     except KeyError:
         continue
-
