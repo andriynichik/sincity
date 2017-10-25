23847f992dec8686a38574869f7410b5b33b164c
bpteam22 <bpteam22@gmail.com>
2017-10-25 23:28:43 +0300

    columns bug fixed

--- client/templates/admin/matching-france/list.js
+++ client/templates/admin/matching-france/list.js
@@ -2,9 +2,9 @@ $(function () {
     var table;
     var i = 0;
     table = $('.js-exportable').removeAttr('width').DataTable({
-        columnDefs: [
-            { width: 200, targets: 0 }
-        ],
+        //columnDefs: [
+        //    { width: 200, targets: 0 }
+        //],
         fixedColumns: true,
         "autoWidth": false,
         "bSortClasses": false,
@@ -69,7 +69,6 @@ $(function () {
 ['{{e(item.get('center', {}).get('lat'))}}','{{e(item.get('center', {}).get('lng'))}}'],
 ['{{e(item.get('bounds', {}).get('left',{}).get('lat'))}}', '{{e(item.get('bounds', {}).get('left',{}).get('lng'))}}', '{{e(item.get('bounds', {}).get('right',{}).get('lat'))}}', '{{e(item.get('bounds', {}).get('right',{}).get('lng'))}}'],
 '{{e(item.get('postal_code'))}}',
-'{{e(item.get('requests'))}}',
 
         {% set item = itemsDic.get('insee', {}) %}
 '{{url_for('insee_code_unit', id=e(item.get('code')))}}',
@@ -97,7 +96,7 @@ $(function () {
         ],
         columnDefs: [
             // INTERNAL
-            {
+            { // INTERAL Edit/Delete
                 render: function ( data, type, row ) {
                     //'<a href="'+ data[0] +'" target="_blank"><i class="material-icons">remove_red_eye</i></a>' +
                     return '<a href="'+ data[1] +'" target="_blank"><i class="material-icons">mode_edit</i></a>' +
@@ -105,118 +104,110 @@ $(function () {
                 },
                 targets: i++
             },
-            {
+            { // INTERNAL Name
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INTERNAL Type
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INTERNAL ADMIN_1
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INTERNAL ADMIN_2
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INTERNAL ADMIN_3
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INTERNAL ADMIN_4
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INTERNAL ADMIN_5
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INTERNAL ADMIN_6
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INTERNAL ADMIN_7
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INTERNAL ADMIN_8
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INTERNAL Capital
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INTERNAL gmap link
                 render: function ( data, type, row ) {
                     return '<a href="'+ data[0] +'" target="_blank">'+ data[1] +'</a>';
                 },
                 targets: i++
             },
-            {
+            { // INTERNAL population
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INTERNAL postal codes
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
             // WIKI
-            {
+            {  // WIKI unit  link
                 render: function ( data, type, row ) {
                     return '<a href="'+ data +'" target="_blank">#</a>';
                 },
                 targets: i++
             },
-            {
+            { // WIKI name
                 render: function ( data, type, row ) {
                     return '<a href="'+ data[1] +'" target="_blank">'+ data[0] +'</a>';
                 },
                 targets: i++
             },
-            {
+            { // WIKI type
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            //{
-            //    render: function ( data, type, row ) {
-            //        return data.reduce(function(previousValue, data, index) {
-            //            return previousValue + '<a href="'+ data[0] +'" target="_blank">'+ data[1] +': '+ data[2] +'</a>';
-            //        }, '')
-            //    },
-            //    targets: i++
-            //},
-            {
+            { // WIKI admin hierarchy
                 render: function ( data, type, row ) {
                     return data.reduce(function(previousValue, data, index) {
                         return previousValue + '[<a href="'+ data[0] +'" target="_blank">'+ data[1] +'('+ data[2] +')</a>]';
@@ -224,92 +215,92 @@ $(function () {
                 },
                 targets: i++
             },
-            {
+            { // WIKI capital
                 render: function ( data, type, row ) {
                     return '<a href="'+ data[0] +'" target="_blank">'+ data[1] +'</a>';
                 },
                 targets: i++
             },
-            {
+            { // WIKI gmap link
                 render: function ( data, type, row ) {
                     return '<a href="'+ data[0] +'" target="_blank">'+ data[1] +'</a>';
                 },
                 targets: i++
             },
-            {
+            { // WIKI altitude
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // WIKI population
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // WIKI density
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // WIKI area
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // WIKI postal codes
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            {  // WIKI communes
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // WIKI canton codes
+                render: function ( data, type, row ) {
+                    return data;
+                },
+                targets: i++
+            },
+            { // WIKI commune codes
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
             //GMAP
-            {
+            { // GMAP unit link
                 render: function ( data, type, row ) {
                     return '<a href="'+ data +'" target="_blank">#</a>';
                 },
                 targets: i++
             },
-            {
+            { // GMAP name
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // GMAP short name
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // GMAP type
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
-                render: function ( data, type, row ) {
-                    return data;
-                },
-                targets: i++
-            },
-            {
+            { // GMAP admin hierarchy
                 render: function ( data, type, row ) {
                     return data.reduce(function(previousValue, data, index) {
                         return previousValue + ' '+ data[0] +'('+ data[1] +') > ';
@@ -317,7 +308,7 @@ $(function () {
                 },
                 targets: i++
             },
-            {
+            { // GMAP center
                 render: function ( data, type, row ) {
                     var lat = data[0];
                     var lng = data[1];
@@ -325,134 +316,128 @@ $(function () {
                 },
                 targets: i++
             },
-            {
+            { // GMAP bounds
                 render: function ( data, type, row ) {
                     return '('+data[0]+','+data[1]+'),('+data[2]+','+data[3]+')';
                 },
                 targets: i++
             },
-            {
-                render: function ( data, type, row ) {
-                    return data;
-                },
-                targets: i++
-            },
-            {
+            { // GMAP postal_code
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
             //INSEE
-            {
+            { // INSEE unit link
                 render: function ( data, type, row ) {
                     return '<a href="'+ data +'" target="_blank">#</a>';
                 },
                 targets: i++
             },
-            {
+            { // INSEE I_Code_Arrondissements
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INSEE I_Ar
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INSEE I_Cheflieu
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INSEE I_Code_canton
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INSEE I_Region
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INSEE I_Dep
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INSEE I_Canton
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INSEE I_Typct
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INSEE I_Burcentral
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INSEE I_Tncc
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INSEE I_Artmaj
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INSEE I_Ncc
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INSEE I_Armin
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INSEE I_Nccent
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INSEE InseeXls_CodeCommune
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INSEE InseeXls_NameCommune
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INSEE InseeXls_Population
                 render: function ( data, type, row ) {
                     return data;
                 },
                 targets: i++
             },
-            {
+            { // INSEE I_Code_departament
                 render: function ( data, type, row ) {
                     return data;
                 },
