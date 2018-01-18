515c459cdb53a874bf893786d82d0f6c50207d74
root <root@dynamic176.local>
2018-01-18 16:41:53 +0200

    fix bug reparse by types

--- console/commands/Spain/reparse_by_types.py
+++ console/commands/Spain/reparse_by_types.py
@@ -70,103 +70,105 @@ config = Config('./config/config.yml')
 mongo_config = config.get('mongodb')
 connection = pymongo.MongoClient(mongo_config['host'], mongo_config['port'])
 db = connection.location
-
-for doc in db.internal.find({'17_gmap_admin_hierarchy.ADMIN_LEVEL_1.name': 'España'}):
-	print (doc['25_SNIG_TIPO'])
-	try:
-		print (doc['12_gmap_type'])
-		types = {"Municipio": "administrative_area_level_4",
-				"Entidad colectiva" :  "administrative_area_level_5",
-				"Otras entidades": "locality",
-				"Capital de municipio": "locality",
-				"Entidad singular": "locality" }
-		if doc['12_gmap_type'] == types[doc['25_SNIG_TIPO']]:
-			print("YESSS")
-		else:
-			print("Noooooooo")
-			# types = {"Municipio": "administrative_area_level_4",
-			# 	"Entidad colectiva" :  "administrative_area_level_5",
-			# 	"Otras entidades": "locality",
-			# 	"Capital de municipio": "locality",
-			# 	"Entidad singular": "locality" } 
-			# place = get_place_ids_by_address_for_type
-			Key = Keygen()
-			keyAPI =  Key.get_key_place()
-			if not keyAPI:
-				sys.exit()
-			cnf = {'googlemaps':{'geocoding':{'key': keyAPI}}}
-			config.set(cnf)
-			print (keyAPI)
-			spider.gmap_loader._language = language
-			print (doc['08_INE_Name_w_Article'], types[doc['25_SNIG_TIPO']])
-			dta = spider.get_place_ids_by_address_for_type(doc['08_INE_Name_w_Article'] + ', España', types[doc['25_SNIG_TIPO']])
-			# gmap = {}
-			# if objects:
-			# 	gmap = objects[0].get_document()
-			if len(dta) > 0:
-				place_id = dta[0]
-				objects = spider.get_gmap_place_id(place_id)
-				gmap = {}
-				gmap = objects[0].get_document()
-				print (gmap)
-				try:
-					if gmap['name'].lower().lstrip().strip() == doc['08_INE_Name_w_Article'].lower().lstrip().strip():
-						gmap['comparison'] = True
-					else:
+try:
+	
+	for doc in db.internal.find({'17_gmap_admin_hierarchy.ADMIN_LEVEL_1.name': 'España'}):
+		print (doc['25_SNIG_TIPO'])
+		try:
+			print (doc['12_gmap_type'])
+			types = {"Municipio": "administrative_area_level_4",
+					"Entidad colectiva" :  "administrative_area_level_5",
+					"Otras entidades": "locality",
+					"Capital de municipio": "locality",
+					"Entidad singular": "locality" }
+			if doc['12_gmap_type'] == types[doc['25_SNIG_TIPO']]:
+				print("YESSS")
+			else:
+				print("Noooooooo")
+				# types = {"Municipio": "administrative_area_level_4",
+				# 	"Entidad colectiva" :  "administrative_area_level_5",
+				# 	"Otras entidades": "locality",
+				# 	"Capital de municipio": "locality",
+				# 	"Entidad singular": "locality" } 
+				# place = get_place_ids_by_address_for_type
+				Key = Keygen()
+				keyAPI =  Key.get_key_place()
+				if not keyAPI:
+					sys.exit()
+				cnf = {'googlemaps':{'geocoding':{'key': keyAPI}}}
+				config.set(cnf)
+				print (keyAPI)
+				spider.gmap_loader._language = language
+				print (doc['08_INE_Name_w_Article'], types[doc['25_SNIG_TIPO']])
+				dta = spider.get_place_ids_by_address_for_type(doc['08_INE_Name_w_Article'] + ', España', types[doc['25_SNIG_TIPO']])
+				# gmap = {}
+				# if objects:
+				# 	gmap = objects[0].get_document()
+				if len(dta) > 0:
+					place_id = dta[0]
+					objects = spider.get_gmap_place_id(place_id)
+					gmap = {}
+					gmap = objects[0].get_document()
+					print (gmap)
+					try:
+						if gmap['name'].lower().lstrip().strip() == doc['08_INE_Name_w_Article'].lower().lstrip().strip():
+							gmap['comparison'] = True
+						else:
+							gmap['comparison'] = False
+					except Exception as e:
 						gmap['comparison'] = False
-				except Exception as e:
-					gmap['comparison'] = False
 
-				gmap['15_GMap_center_SNIG_comparison'] = getDistance(gmap['center']['lat'], gmap['center']['lng'],doc['28_SNIG_LATITUD_ETRS89'],doc['29_SNIG_LONGITUD_ETRS89'])
-				gmap['15_gmap_comparison_url'] =("https://www.google.com.ua/maps/dir/"+str(gmap['center']['lat'])+","+str(gmap['center']['lng'])+"/"+str(doc['28_SNIG_LATITUD_ETRS89'])+","+str(doc['29_SNIG_LONGITUD_ETRS89'])+"")
-				db.internal.update_one(
-		                {"_id": doc['_id']},
-		                    {
-		                        "$set": {
-		                        "10_gmap_name": gmap.get('name'),
-		                        "17_gmap_admin_hierarchy": gmap.get('admin_hierarchy', {}),
-		                        "gmap_center": gmap.get('center'),
-		                        "gmap_bounds": gmap.get('bounds'),
-		                        "12_gmap_type": gmap.get('type'),
-		                        "15_GMap_center_SNIG_comparison": gmap.get('15_GMap_center_SNIG_comparison'),
-		                        "15_gmap_comparison_url": gmap.get('15_gmap_comparison_url'),
-		                        "11_gmap_comparison" : gmap['comparison']
-		                        
-		                    }
-		               }
-		        )
-		        # gmap.pop('_id')
-		        # # gmap['15_GMap_center_SNIG_comparison'] = getDistance(gmap['center']['lat'], gmap['center']['lng'],doc['28_SNIG_LATITUD_ETRS89'],doc['29_SNIG_LONGITUD_ETRS89'])
-		        # if gmap['15_GMap_center_SNIG_comparison'] <= 1:
-		        #     gm_comp_status = True
-		        # else:
-		        #     gm_comp_status = False
+					gmap['15_GMap_center_SNIG_comparison'] = getDistance(gmap['center']['lat'], gmap['center']['lng'],doc['28_SNIG_LATITUD_ETRS89'],doc['29_SNIG_LONGITUD_ETRS89'])
+					gmap['15_gmap_comparison_url'] =("https://www.google.com.ua/maps/dir/"+str(gmap['center']['lat'])+","+str(gmap['center']['lng'])+"/"+str(doc['28_SNIG_LATITUD_ETRS89'])+","+str(doc['29_SNIG_LONGITUD_ETRS89'])+"")
+					db.internal.update_one(
+			                {"_id": doc['_id']},
+			                    {
+			                        "$set": {
+			                        "10_gmap_name": gmap.get('name'),
+			                        "17_gmap_admin_hierarchy": gmap.get('admin_hierarchy', {}),
+			                        "gmap_center": gmap.get('center'),
+			                        "gmap_bounds": gmap.get('bounds'),
+			                        "12_gmap_type": gmap.get('type'),
+			                        "15_GMap_center_SNIG_comparison": gmap.get('15_GMap_center_SNIG_comparison'),
+			                        "15_gmap_comparison_url": gmap.get('15_gmap_comparison_url'),
+			                        "11_gmap_comparison" : gmap['comparison']
+			                        
+			                    }
+			               }
+			        )
+			        # gmap.pop('_id')
+			        # # gmap['15_GMap_center_SNIG_comparison'] = getDistance(gmap['center']['lat'], gmap['center']['lng'],doc['28_SNIG_LATITUD_ETRS89'],doc['29_SNIG_LONGITUD_ETRS89'])
+			        # if gmap['15_GMap_center_SNIG_comparison'] <= 1:
+			        #     gm_comp_status = True
+			        # else:
+			        #     gm_comp_status = False
 
-		        # types = {"Municipio": ["administrative_area_level_4"],
-		        #     "Entidad colectiva" :  ["administrative_area_level_5", "neighborhood"],
-		        #     "Otras entidades": ["locality", "neighborhood"],
-		        #     "Capital de municipio":["locality"],
-		        #     "Entidad singular": ["locality"]}
-		        # if gmap.get('type') in types[doc['25_SNIG_TIPO']]:
-		        #     gm_type_status = True
-		        # else:
-		        #     gm_type_status = False
+			        # types = {"Municipio": ["administrative_area_level_4"],
+			        #     "Entidad colectiva" :  ["administrative_area_level_5", "neighborhood"],
+			        #     "Otras entidades": ["locality", "neighborhood"],
+			        #     "Capital de municipio":["locality"],
+			        #     "Entidad singular": ["locality"]}
+			        # if gmap.get('type') in types[doc['25_SNIG_TIPO']]:
+			        #     gm_type_status = True
+			        # else:
+			        #     gm_type_status = False
 
-		        # raw = {
-		        #         "gmap_name": gmap.get('name'),
-		        #         "gmap_name_status" : gmap['comparison'],
-		        #         "gmap_type": gmap.get('type'),
-		        #         "15_GMap_center_SNIG_comparison": gmap.get('15_GMap_center_SNIG_comparison'),
-		        #         "15_gmap_comparison_url": gmap.get('15_gmap_comparison_url'),
-		        #         "gmap_comp_status":gm_comp_status,
-		        #         "gmap_type_status":gm_type_status,
+			        # raw = {
+			        #         "gmap_name": gmap.get('name'),
+			        #         "gmap_name_status" : gmap['comparison'],
+			        #         "gmap_type": gmap.get('type'),
+			        #         "15_GMap_center_SNIG_comparison": gmap.get('15_GMap_center_SNIG_comparison'),
+			        #         "15_gmap_comparison_url": gmap.get('15_gmap_comparison_url'),
+			        #         "gmap_comp_status":gm_comp_status,
+			        #         "gmap_type_status":gm_type_status,
 
-		        #     }
-			print (objects)
-	except Exception as e:
-		print(str(e))
+			        #     }
+				print (objects)
+		except Exception as e:
+			print(str(e))
 
-
+except Exception as e:
+	raise print(str(e))
 
 
 	print ("===================================================")
