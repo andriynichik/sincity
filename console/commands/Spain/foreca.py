fc5788ac9f02a1626e400ffde66c88537b30a2dc
root <root@dynamic176.local>
2018-04-04 19:18:25 +0300

    foreca fix

--- console/commands/Spain/foreca.py
+++ console/commands/Spain/foreca.py
@@ -19,13 +19,19 @@ coll = db.SPAININE
 
 
 csv.register_dialect('myDialect', delimiter=',', quoting=csv.QUOTE_NONE)
-myFile = open('foreca.csv', 'w')  
+myFile = open('foreca_spain.csv', 'w')  
 print(db.internal.find( { 'sinoptik_db_id' : {'$exists': True } } ).count())
 with myFile:
     writer = csv.writer(myFile, dialect='myDialect')
 
     for row in  db.internal.find( { 'sinoptik_db_id' : {'$exists': True } } ):
-        writer.writerows([[row['sinoptik_db_id'],row['29_SNIG_LONGITUD_ETRS89'], row['28_SNIG_LATITUD_ETRS89'], row['27_SNIG_ALTITUD'], row['25_SNIG_TIPO']]])
+        if row['25_SNIG_TIPO'] == 'Entidad singular' or row['25_SNIG_TIPO'] == 'Otras entidades' or row['25_SNIG_TIPO'] == 'Capital de municipio':
+            if str(row['29_SNIG_LONGITUD_ETRS89']) != '0.0' and str(row['28_SNIG_LATITUD_ETRS89']) != '0.0':
+
+                try:
+                    writer.writerows([[row['sinoptik_db_id'],row['29_SNIG_LONGITUD_ETRS89'], row['28_SNIG_LATITUD_ETRS89'], row['27_SNIG_ALTITUD'], row['25_SNIG_TIPO']]])
+                except Exception as e:
+                    print (str(e))
                         # writer.writerows([[row['29_SNIG_LONGITUD_ETRS89']]])
                         # writer.writerows([[row['28_SNIG_LATITUD_ETRS89']]])
                         # writer.writerows([[row['27_SNIG_ALTITUD']]])
