1c4f0a3cf8ec33ab31b87b7c978e0e3fea1d54fe
root <root@dynamic176.local>
2018-04-18 13:35:32 +0300

    coordinate Belarus

--- console/commands/belarus/coordinate.py
+++ console/commands/belarus/coordinate.py
@@ -4,6 +4,8 @@ import pandas as pd
 import json
 import requests
 from pymongo import MongoClient
+from bson.objectid import ObjectId
+
 # from pymongo import Connection
 config = Config('./config/config.yml')
 mongo_config = config.get('mongodb')
@@ -13,15 +15,22 @@ db = conn.location
 coll = db.belarus
     
 for index, row in df.iterrows():
-    print (row)
-    # data = {'OBJECTNUMBER': int(row[0]),
-    #         'SOATO':row[1],
-    #         'NAMEOBJECT':row[2],
-    #         'NAMEOBJBELRUS':row[3],
-    #         'NAMEREGION':row[4],
-    #         'NAMEDISTR':row[5],
-    #         'NAMESELSOVET':row[6],
-    #         'CENTERATE':row[7],
-    #     }
-    # print (data)
+    print (row[2])
+    data = {'lat': row[1],
+            'lng':row[0],
+            
+
+        }
+    print (data)
+    db.belarus.update_one(
+                            {"OBJECTNUMBER": row[2]},
+                                {
+                                    "$set": {
+                                        
+                                          'lat': row[1],
+                                          'lng': row[0],
+                                        
+                                }
+                            }
+                        )
     # coll.save(data)
