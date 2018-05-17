381825b81195903b8d0bd4adb7dfe7d16f7a324b
root <root@dynamic176.local>
2018-05-17 18:59:57 +0300

    belarus st gooole

--- client/application/index.py
+++ client/application/index.py
@@ -1401,6 +1401,129 @@ def romania_reparse_by_geocode():
 
 
 
+@app.route('/belarus-st_by_geocode', methods=['GET', 'POST'])
+@login_required
+def belarus_st_by_geocode():
+
+    # return render_template('admin/gmap/list.html', country=country)
+    config = Config('./config/config.yml')
+    mongo_config = config.get('mongodb')
+    # use_cache = bool(get.get('use_cache', True))
+    connection = MongoClient(mongo_config['host'], mongo_config['port'])
+    db = connection.location
+    doc = db.belarus_st.find_one({"_id" : ObjectId(request.form['id']) })
+    doc_factory = DocFactory(config.get('mongodb'))
+    Key = Keygen()
+    keyAPI =  Key.get_key_place()
+    if not keyAPI:
+        sys.exit()
+    cnf = {'googlemaps':{'geocoding':{'key': keyAPI}}}
+    config.set(cnf)
+    gmap_config = config.get('googlemaps')
+    # gmap_config.update(language=language)
+    language = 'ru' 
+
+    gmap_loader = LoaderFactory.loader_gmaps_with_cache(gmaps_config=gmap_config,
+                                                            storage_config=config.get('mongodb'))
+    spider = Spider(
+            loader_factory=LoaderFactory,
+            gmap_parser=MapFactory.spain,
+            doc_factory=doc_factory,
+            language=language,
+            config=config,
+            use_cache=True
+    )
+    if request.form['type'] == "autocomplete":
+        if 'NAMESELSOVET' in doc:
+            raw = gmap_loader.by_places(doc['NAMEOBJECT'] + ', '+str(doc['NAMESELSOVET']))
+        else:
+            raw = gmap_loader.by_places(str(doc['NAMEOBJECT']))
+        return json.dumps(raw)
+    else:
+        objects = spider.get_gmap_place_id(request.form['place_id'])
+        gmap = {}
+        gmap = objects[0].get_document()
+        try:
+            if gmap['name'] == doc['NAMEOBJECT']:
+                gmap['comparison'] = True
+            else:
+                gmap['comparison'] = False
+        except Exception as e: 
+            gmap['comparison'] = False
+
+
+
+        if 'XCoord' in doc:
+            
+            distance =  getDistance(gmap['center']['lat'], gmap['center']['lng'], doc["XCoord"],doc["YCoord"])
+            # gmap['15_GMap_center_SNIG_comparison'] = getDistance(gmap['center']['lat'], gmap['center']['lng'],doc['28_SNIG_LATITUD_ETRS89'],doc['29_SNIG_LONGITUD_ETRS89'])
+            gmap['gmap_comparison_url'] =("https://www.google.com.ua/maps/dir/"+str(gmap['center']['lat'])+","+str(gmap['center']['lng'])+"/"+str(doc["XCoord"])+","+str(doc["YCoord"])+"")
+        
+        else:
+            distance =  False
+            # gmap['15_GMap_center_SNIG_comparison'] = getDistance(gmap['center']['lat'], gmap['center']['lng'],doc['28_SNIG_LATITUD_ETRS89'],doc['29_SNIG_LONGITUD_ETRS89'])
+            gmap['gmap_comparison_url'] = False
+      
+
+        db.belarus_st.update_one(
+                {"_id": ObjectId(request.form['id']) },
+                    {
+                        "$set": {
+                                'gmap_name': gmap.get('name'),
+                                'gmap_admin_hierarchy': gmap.get('admin_hierarchy', {}),
+                                'gmap_center': gmap.get('center'),
+                                'gmap_bounds': gmap.get('bounds'),
+                                'gmap_type': gmap.get('type'),
+                                'gmap_translate': gmap.get('translate'),
+                                'gmap_requests': gmap.get('requests'),
+                                'gmap_code': gmap.get('code'),
+                                'gmap_postal_code': gmap.get('postal_code'),
+                                'gmap_st_distance':distance
+
+                    }
+               }
+        )
+
+        if distance < 2:
+            distance_status = True
+        else:
+            distance_status = False
+        # gmap.pop('_id')
+        # # gmap['15_GMap_center_SNIG_comparison'] = getDistance(gmap['center']['lat'], gmap['center']['lng'],doc['28_SNIG_LATITUD_ETRS89'],doc['29_SNIG_LONGITUD_ETRS89'])
+        # if gmap['15_GMap_center_SNIG_comparison'] <= 1:
+        #     gm_comp_status = True
+        # else:
+        #     gm_comp_status = False
+
+        # types = {"Municipio": ["administrative_area_level_4"],
+        #     "Entidad colectiva" :  ["administrative_area_level_5", "neighborhood"],
+        #     "Otras entidades": ["locality", "neighborhood"],
+        #     "Capital de municipio":["locality"],
+        #     "Entidad singular": ["locality"]}
+        # if gmap.get('type') in types[doc['25_SNIG_TIPO']]:
+        #     gm_type_status = True
+        # else:
+        #     gm_type_status = False
+
+
+
+
+            
+        raw = {
+                "gmap_name": gmap.get('name'),
+                "gmap_name_status" : gmap['comparison'],
+                "gmap_type": gmap.get('type'),
+      
+ 
+                "distance":distance,
+                "distance_status":distance_status,
+                "gmap_comparison_url":gmap['gmap_comparison_url'],
+                'gmap_type': gmap.get('type'),
+
+            }
+        
+        return json.dumps(raw)
+
 @app.route('/belarus-reparse_by_geocode', methods=['GET', 'POST'])
 @login_required
 def belarusreparse_by_geocode():
--- console/commands/belarus/st_coordinate.py
+++ console/commands/belarus/st_coordinate.py
@@ -11,26 +11,26 @@ from bson.objectid import ObjectId
 # from pymongo import Connection
 config = Config('./config/config.yml')
 mongo_config = config.get('mongodb')
-df = pd.read_csv('./data/belarus/st_wgs.csv',  skiprows=0, low_memory=False) 
+df = pd.read_csv('./data/belarus/bel_st_utf8.csv',  skiprows=0, low_memory=False) 
 conn =  MongoClient(mongo_config['host'], mongo_config['port'])
 db = conn.location
 coll = db.belarus_st
     
 for index, row in df.iterrows():
     # print (row[2])
-    data = {'lat': row[5],
-            'lng':row[6],
+    data = {'NameOblect_C50': row[1],
+            'NameSettle_C50':row[4],
+            'id':int(float(row[0].replace(',', '.')))
             
 
         }
     print (data)
     db.belarus_st.update_one(
-                            {"OBJECTNUMBER": int(row[0])},
+                            {"OBJECTNUMBER": int(float(row[0].replace(',', '.')))},
                                 {
                                     "$set": {
-                                        
-                                          'XCoord': row[5],
-                                          'YCoord': row[6],
+                                        'NameOblect_C50':row[1],
+                                        'NameSettle_C50':row[4],
                                         
                                 }
                             }
